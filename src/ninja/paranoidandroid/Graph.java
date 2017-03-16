5793281e455284dc340037a1ea1d3f75921bbebc
Anton Simeonov <antonmitkovsimeonov@gmail.com>
2017-03-16 15:54:31 +0200

    Initial commit.

---
+++ src/ninja/paranoidandroid/Graph.java
@@ -0,0 +1,26 @@
+package ninja.paranoidandroid;
+
+import java.util.List;
+
+/**
+ * Created by anton on 16.03.17.
+ */
+public class Graph {
+
+    private final List<Vertex> vertexes;
+    private final List<Edge> edges;
+
+    public Graph(List<Vertex> vertexes, List<Edge> edges) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+    }
+
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+}
